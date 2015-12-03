daeab25089f3ab2e1b3070e2d310daaa1cd820c3
vanshajr-cuelogic <vanshaj.rai@cuelogic.co.in>
2015-12-03 17:11:17 +0530

    Day-4 assignment

--- HTML-Training/Day-4/js/master.js
+++ HTML-Training/Day-4/js/master.js
@@ -1,11 +1,29 @@
 $(document).ready(function(){
-	$(".video-blog").hide();
+
+	var c = document.getElementById("canvas_text");
+	var context = c.getContext("2d");
+	context.font = "30px Arial";
+	context.fillStyle = '#fff';
+	context.fillText("Bingo",5,25);
+
+
+	$(".site-blog-section").hide();
+	$(".video-blog-section").hide();
+
 	$(".img-blog").click(function(){
 		$(".site-blog-section").hide();
+		$(".video-blog-section").hide();
 		$(".image-blog-section").show();
 	});
 	$(".site-blog").click(function(){
 		$(".image-blog-section").hide();
+		$(".video-blog-section").hide();
 		$(".site-blog-section").show();	
 	})
-})
+	$(".video-blog").click(function(){
+		$(".site-blog-section").hide();
+		$(".image-blog-section").hide();
+		$(".video-blog-section").show();
+
+	});
+});
