38fcbe288d5dc9d58a4d33df3a8c0963c56f1c25
vanshajr-cuelogic <vanshaj.rai@cuelogic.co.in>
2016-01-11 19:43:05 +0530

    Day1-First practical

---
+++ JS-Training/Day-1/index.js
@@ -0,0 +1,7 @@
+var num;
+var name = "Attitude";
+var boolean;
+var object;
+var array;
+
+document.getElementById("container").innerHTML = name;
---
+++ JS-Training/Day-1/js/index.js
@@ -0,0 +1,8 @@
+var num;
+var name = "Attitude";
+var boolean;
+var object;
+var array;
+
+document.getElementById("demo").innerHTML = name;
+console.log(name);
