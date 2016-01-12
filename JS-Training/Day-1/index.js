77253088f573adc251f556db5d9b7b9d6deccc96
vanshajr-cuelogic <vanshaj.rai@cuelogic.co.in>
2016-01-12 19:29:13 +0530

    Day one Assignment

--- JS-Training/Day-1/index.js
+++
@@ -1,7 +0,0 @@
-var num;
-var name = "Attitude";
-var boolean;
-var object;
-var array;
-
-document.getElementById("container").innerHTML = name;
--- JS-Training/Day-1/js/index.js
+++
@@ -1,8 +0,0 @@
-var num;
-var name = "Attitude";
-var boolean;
-var object;
-var array;
-
-document.getElementById("demo").innerHTML = name;
-console.log(name);
